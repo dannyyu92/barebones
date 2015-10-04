e344e4f777b2884e41ef35f20a4abf987f2ae7d1
Danny Yu <dannyyu92@gmail.com>
2015-10-03 23:59:20 -0700

    Update ping endpoint to return a success

--- templates/configs_controller.rb
+++ templates/configs_controller.rb
@@ -1,7 +1,5 @@
 class Api::V1::ConfigsController < Api::V1::ApplicationController
   def ping
-    @error_msg = "Test error message."
-    @errors = ["TestError1", "TestError2"]
-    return render_api_fail("configs/ping")
+    return render_api_success("configs/ping")
   end
 end
